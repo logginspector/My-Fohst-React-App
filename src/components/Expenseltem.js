e0d71368ce0f0ef3c4566064bf276650409671dc
MisterScript <staley77@protonmail.com>
2022-08-12 22:25:00 -0400

    add props and multi-component

--- src/components/Expenseltem.js
+++ src/components/Expenseltem.js
@@ -1,6 +1,18 @@
-function Expenseltem() {
-    return
-    <div>Date</div>
+import './Expenseltem.css'
+
+function Expenseltem(props) {
+    const expenseDate = new Date(2022, 7, 11);
+    const expenseTitle = 'Car lnsurance'
+    const expenseAmount = 294.42069;
+    return (
+        <div className="expense-item">
+            <div> {props.date} </div>
+            <div className="expense-item__">
+                <h2> {props.title} </h2>
+                <div className="expense-item__price">{expenseAmount}</div>
+            </div>
+        </div>
+    );
 }
 
 export default Expenseltem
